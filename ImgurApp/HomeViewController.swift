d4b0dfcf1afcf86530c1c13ddf1e689055da7b90
ferrannl <fhendrik1@avans.nl>
2021-03-21 10:24:21 -0700

    added HomeViewController

---
+++ ImgurApp/HomeViewController.swift
@@ -0,0 +1,49 @@
+//
+//  HomeViewController.swift
+//  ImgurApp
+//
+//  Created by Ferran Hendriks on 21/03/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let images = [
+    "https://i.imgur.com/i7SQKwO.jpeg",
+    "https://i.imgur.com/uHvF70X.jpeg"
+    ]
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+        
+    func parseData(){
+        let url = ""
+    }
+    
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped me!")
+    }
+    
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = images[indexPath.row]
+        
+    return cell
+    }
+        
+
+}
--- ImgurApp/ViewController.swift
+++ ImgurApp/ViewController.swift
@@ -9,51 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let images = [
-    "https://i.imgur.com/i7SQKwO.jpeg",
-    "https://i.imgur.com/uHvF70X.jpeg"
-    ]
-    
-    @IBOutlet var tableView: UITableView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
     }
-    
-    @IBAction func didTapUploadButton(){
-        return
-    }
-    
-    func parseData(){
-        let url = ""
-    }
-    
 }
-
-extension ViewController: UITableViewDelegate{
-    
-   
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("You tapped me!")
-    }
-    
-}
-
-extension ViewController: UITableViewDataSource{
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return images.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        cell.textLabel?.text = images[indexPath.row]
-        
-    return cell
-    }
-        
-}
-
