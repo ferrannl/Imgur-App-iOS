24ab17b199bfb06b8b3c816a223b8f73f1b893f5
ferrannl <fhendrik1@avans.nl>
2021-03-21 05:48:10 -0700

    Initial Commit

---
+++ ImgurApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImgurApp
+//
+//  Created by Ferran Hendriks on 21/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
