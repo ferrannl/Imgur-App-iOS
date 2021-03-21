7e8b31e9fd265de727e365c425116bf6aa5999d6
ferrannl <fhendrik1@avans.nl>
2021-03-21 10:01:37 -0700

    temp links

--- ImgurApp/ViewController.swift
+++ ImgurApp/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let images = [
+    "https://i.imgur.com/i7SQKwO.jpeg",
+    "https://i.imgur.com/uHvF70X.jpeg"
+    ]
+    
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -29,6 +34,7 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDelegate{
     
+   
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("You tapped me!")
     }
@@ -38,13 +44,13 @@ extension ViewController: UITableViewDelegate{
 extension ViewController: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return images.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        cell.textLabel?.text = "Hello World"
+        cell.textLabel?.text = images[indexPath.row]
         
     return cell
     }
