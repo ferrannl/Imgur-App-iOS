3977493690ccb56913ee8421a2da89c05b76c8b8
ferrannl <fhendrik1@avans.nl>
2021-03-21 10:27:26 -0700

    Added Controllers

---
+++ ImgurApp/SettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.swift
+//  ImgurApp
+//
+//  Created by Ferran Hendriks on 21/03/2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ImgurApp/UploadViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UploadViewController.swift
+//  ImgurApp
+//
+//  Created by Ferran Hendriks on 21/03/2021.
+//
+
+import UIKit
+
+class UploadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
